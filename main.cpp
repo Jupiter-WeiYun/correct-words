3c25762b67a8e036df2a1d7ea4ec5c2510c06632
Jupiter-WeiYun <125134601+Jupiter-WeiYun@users.noreply.github.com>
2023-02-20 10:55:07 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <cstdio>
 #include <cstdlib>
+#include <string>
+#include <vector>
 #include "function\write\write.h"
 
 using namespace std;
 
+string word;
+vector<string> wordList;
+
 int main(){
     
     while(true){
-        
+        cin >> word;
     }
     return 0;
 }
